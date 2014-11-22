6a6bb0a40c431f71052dc4092de7a1fc7ab1f943
Alexander Baier <alexander.baier@mailbox.org>
2014-11-22 22:59:12 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("main-nav").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='introduction.html'>Einführung</a></li>" +
+    // "<li><a href='idea.html'>Idee</a></li>" +
+    // "<li><a href='development.html'>Entwicklung</a></li>" +
+    // "<li><a href='organisation.html'>Organisation</a></li>" +
+    // "<li><a href='name.html'>Namensgebung</a></li>" +
+    // "<li><a href='specs.html'>Spezifikation</a></li>" +
+    "</ul>";
