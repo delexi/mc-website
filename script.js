15c8fcb3ae29feef548cd7a3f41c312c8a036dcf
Alexander Baier <alexander.baier@mailbox.org>
2014-11-24 19:32:39 +0100

    Include all pages in the menu.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("main-nav").innerHTML =
     "<ul id='menu'>" +
     "<li><a href='index.html'>Home</a></li>" +
     "<li><a href='introduction.html'>Einführung</a></li>" +
-    // "<li><a href='idea.html'>Idee</a></li>" +
-    // "<li><a href='development.html'>Entwicklung</a></li>" +
-    // "<li><a href='organisation.html'>Organisation</a></li>" +
-    // "<li><a href='name.html'>Namensgebung</a></li>" +
-    // "<li><a href='specs.html'>Spezifikation</a></li>" +
+    "<li><a href='idea.html'>Idee</a></li>" +
+    "<li><a href='development.html'>Entwicklung</a></li>" +
+    "<li><a href='organisation.html'>Organisation</a></li>" +
+    "<li><a href='name.html'>Namensgebung</a></li>" +
+    "<li><a href='specs.html'>Spezifikation</a></li>" +
     "</ul>";
